baf1ed6e7dafcbb0c38850d4ee353a408cca2582
Priyanshu Mishra <priyanshu1928.be21@chitkara.edu.in>
2024-08-08 11:03:12 +0530

    connect to mongodb Atlas1

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
 import dotenv from 'dotenv'
-import connectDB from './db/index.js'
-
+import connectDB from "./db/index.js";
 dotenv.config({
-    path : './env'
+    path: './.env'
 })
 
-connectDB();
+
+
+connectDB()
+
+
+/*
+import express from 'express'
+
+const app = express();
+
+(async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("Error" , ()=> {
+            console.log("Error: ", error);
+            throw error;
+        })
+        app.listen(process.env.PORT , ()=> {
+            console.log(`App is listening at the port ${process.env.PORT}`);
+        })
+    } catch (error) {
+        console.error("Error: ", error);
+        throw error;
+    }
+})()
+*/
