76c6cd3c61f6411207d71ac7bf4f8c22c1ae985e
Priyanshu Mishra <priyanshu1928.be21@chitkara.edu.in>
2024-08-18 09:35:41 +0530

    authorization middleware for verifying jwt token for logout

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,26 @@
+import { ApiError } from "../utils/ApiError";
+import { asyncHandler } from "../utils/asyncHandler";
+import jwt from "jsonwebtoken"
+import { User } from "../models/users.model.js";
+
+export const verifyJWT = asyncHandler(async (req,res,next) => {
+    try {
+        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer " , "")  // here in this authorization is a key value pair in this in value we have Bearer and then token number
+    
+        if(!token){
+            throw new ApiError(401 , "Unauthorized request")
+        }
+    
+        const decodedtoken = await jwt.verify(token , process.env.ACCESS_TOKEN_SECRET)
+    
+        const user = User.findById(decodedtoken?._id).select("-password -refreshToken")
+    
+        if(!user){
+            throw new ApiError(401, "Invalid Access Token")
+        }
+        req.user = user
+        next()
+    } catch (error) {
+        throw new ApiError(401 , error?.message || "Invalid access Token")
+    }
+})
